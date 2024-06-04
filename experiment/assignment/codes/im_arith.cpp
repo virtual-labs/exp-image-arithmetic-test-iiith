8a9326dc15a253b042e18d004dc88ed03aa4edac
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-06-04 14:26:19 +0530

    Updated code to PHP 8 and OpenCV 4

--- experiment/assignment/codes/im_arith.cpp
+++ experiment/assignment/codes/im_arith.cpp
@@ -1,425 +1,324 @@
-#include<iostream>
-#include<stdio.h>
-#include <string.h>
-#include<opencv/cv.h>
-#include<cmath>
-#include<opencv/highgui.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
 
-IplImage* imADD(IplImage *img1,IplImage *img2,int map_fn)
+Mat imADD(const Mat &img1, const Mat &img2, int map_fn)
 {
-	int c = cvGetSize(img1).width;
-	int r = cvGetSize(img1).height;
-	
-	IplImage* final_img=cvCreateImage(cvSize(c,r),IPL_DEPTH_8U,1);
-	if(map_fn==1)
+	int c = img1.cols;
+	int r = img1.rows;
+
+	Mat final_img(r, c, CV_8U);
+	if (map_fn == 1)
 	{
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = (int)(s1.val[0] + s2.val[0]);
-				if(grval > 255)
-					grval=255;
-				s.val[0] = grval;
-				cvSet2D(final_img,i,j,s);
+				int grval = img1.at<uchar>(i, j) + img2.at<uchar>(i, j);
+				if (grval > 255)
+					grval = 255;
+				final_img.at<uchar>(i, j) = grval;
 			}
 		}
-
 	}
-	else if(map_fn==2)
+	else if (map_fn == 2)
 	{
-		int min_val=100000;
-		int max_val=-300;
+		int min_val = 100000;
+		int max_val = -300;
 
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = (int)(s1.val[0] + s2.val[0]);
-				if(grval > max_val)
+				int grval = img1.at<uchar>(i, j) + img2.at<uchar>(i, j);
+				if (grval > max_val)
 					max_val = grval;
-				if(grval < min_val)
+				if (grval < min_val)
 					min_val = grval;
 			}
 		}
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = (int)(s1.val[0] + s2.val[0]);
-				if(max_val==0 and min_val==0)
+				int grval = img1.at<uchar>(i, j) + img2.at<uchar>(i, j);
+				if (max_val == 0 && min_val == 0)
 					grval = 0;
 				else
-					grval = (int)(((grval - min_val)*255)/(max_val - min_val));
-				s.val[0] = grval;
-				cvSet2D(final_img,i,j,s);
+					grval = ((grval - min_val) * 255) / (max_val - min_val);
+				final_img.at<uchar>(i, j) = grval;
 			}
 		}
-
 	}
 	return final_img;
 }
 
-IplImage* imSUB(IplImage *img1,IplImage *img2,int map_fn)
+Mat imSUB(const Mat &img1, const Mat &img2, int map_fn)
 {
-	int c = cvGetSize(img1).width;
-	int r = cvGetSize(img1).height;
-	
-	IplImage* final_img=cvCreateImage(cvSize(c,r),IPL_DEPTH_8U,1);
-	if(map_fn==1)
+	int c = img1.cols;
+	int r = img1.rows;
+
+	Mat final_img(r, c, CV_8U);
+	if (map_fn == 1)
 	{
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = (int)(s1.val[0] - s2.val[0]);
-				if(grval < 0)
-					grval=0;
-				s.val[0] = grval;
-				cvSet2D(final_img,i,j,s);
+				int grval = img1.at<uchar>(i, j) - img2.at<uchar>(i, j);
+				if (grval < 0)
+					grval = 0;
+				final_img.at<uchar>(i, j) = grval;
 			}
 		}
-
 	}
-	else if(map_fn==2)
+	else if (map_fn == 2)
 	{
-		int min_val=100000;
-		int max_val=-300;
+		int min_val = 100000;
+		int max_val = -300;
 
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = (int)(s1.val[0] - s2.val[0]);
-				if(grval > max_val)
+				int grval = img1.at<uchar>(i, j) - img2.at<uchar>(i, j);
+				if (grval > max_val)
 					max_val = grval;
-				if(grval < min_val)
+				if (grval < min_val)
 					min_val = grval;
 			}
 		}
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = (int)(s1.val[0] - s2.val[0]);
-				if(max_val==0 and min_val==0)
+				int grval = img1.at<uchar>(i, j) - img2.at<uchar>(i, j);
+				if (max_val == 0 && min_val == 0)
 					grval = 0;
 				else
-					grval = (int)((grval - min_val)*255/(max_val - min_val));
-				s.val[0] = grval;
-				cvSet2D(final_img,i,j,s);
+					grval = ((grval - min_val) * 255) / (max_val - min_val);
+				final_img.at<uchar>(i, j) = grval;
 			}
 		}
-
-	}
-	return final_img;
-}
-IplImage* imDIFF(IplImage *img1,IplImage *img2,int map_fn)
-{
-	int c = cvGetSize(img1).width;
-	int r = cvGetSize(img1).height;
-	
-	IplImage* final_img=cvCreateImage(cvSize(c,r),IPL_DEPTH_8U,1);
-	if(map_fn==1)
-	{
-		for(int i=0;i<r;i++)
-		{
-			for(int j=0;j<c;j++)
-			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = abs(s1.val[0] - s2.val[0]);
-				s.val[0] = grval;
-				cvSet2D(final_img,i,j,s);
-			}
-		}
-
-	}
-	else if(map_fn==2)
-	{
-		int min_val=100000;
-		int max_val=-300;
-
-		for(int i=0;i<r;i++)
-		{
-			for(int j=0;j<c;j++)
-			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = abs(s1.val[0] - s2.val[0]);
-				if(grval > max_val)
-					max_val = grval;
-				if(grval < min_val)
-					min_val = grval;
-			}
-		}
-		for(int i=0;i<r;i++)
-		{
-			for(int j=0;j<c;j++)
-			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = abs(s1.val[0] - s2.val[0]);
-				if(max_val==0 and min_val==0)
-					grval = 0;
-				else
-					grval = (int)((grval - min_val)*255/(max_val - min_val));
-				s.val[0] = grval;
-				cvSet2D(final_img,i,j,s);
-			}
-		}
-
 	}
 	return final_img;
 }
 
-IplImage* imMULTIPLY(IplImage *img1,IplImage *img2,int map_fn)
+Mat imDIFF(const Mat &img1, const Mat &img2, int map_fn)
 {
-	int c = cvGetSize(img1).width;
-	int r = cvGetSize(img1).height;
-	
-	IplImage* final_img=cvCreateImage(cvSize(c,r),IPL_DEPTH_8U,1);
-	if(map_fn==1)
+	int c = img1.cols;
+	int r = img1.rows;
+
+	Mat final_img(r, c, CV_8U);
+	if (map_fn == 1)
 	{
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = (int)(s1.val[0] * s2.val[0]);
-				if(grval > 255)
-					grval=255;
-				s.val[0] = grval;
-				cvSet2D(final_img,i,j,s);
+				int grval = abs(img1.at<uchar>(i, j) - img2.at<uchar>(i, j));
+				final_img.at<uchar>(i, j) = grval;
 			}
 		}
-
 	}
-	else if(map_fn==2)
+	else if (map_fn == 2)
 	{
-		int min_val=100000;
-		int max_val=-300;
+		int min_val = 100000;
+		int max_val = -300;
 
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = (int)(s1.val[0] * s2.val[0]);
-				if(grval > max_val)
+				int grval = abs(img1.at<uchar>(i, j) - img2.at<uchar>(i, j));
+				if (grval > max_val)
 					max_val = grval;
-				if(grval < min_val)
+				if (grval < min_val)
 					min_val = grval;
 			}
 		}
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
-				int grval = (int)(s1.val[0] * s2.val[0]);
-				if(max_val==0 and min_val==0)
+				int grval = abs(img1.at<uchar>(i, j) - img2.at<uchar>(i, j));
+				if (max_val == 0 && min_val == 0)
 					grval = 0;
 				else
-					grval = (int)((grval - min_val)*255/(max_val - min_val));
-				s.val[0] = grval;
-				cvSet2D(final_img,i,j,s);
+					grval = ((grval - min_val) * 255) / (max_val - min_val);
+				final_img.at<uchar>(i, j) = grval;
 			}
 		}
-
 	}
 	return final_img;
 }
 
-IplImage* imDIVIDE(IplImage *img1,IplImage *img2,int map_fn)
+Mat imMULTIPLY(const Mat &img1, const Mat &img2, int map_fn)
 {
-	int c = cvGetSize(img1).width;
-	int r = cvGetSize(img1).height;
-	
-	IplImage* final_img=cvCreateImage(cvSize(c,r),IPL_DEPTH_8U,1);
-	if(map_fn==1)
+	int c = img1.cols;
+	int r = img1.rows;
+
+	Mat final_img(r, c, CV_8U);
+	if (map_fn == 1)
 	{
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
+				int grval = img1.at<uchar>(i, j) * img2.at<uchar>(i, j);
+				if (grval > 255)
+					grval = 255;
+				final_img.at<uchar>(i, j) = grval;
+			}
+		}
+	}
+	else if (map_fn == 2)
+	{
+		int min_val = 100000;
+		int max_val = -300;
+
+		for (int i = 0; i < r; i++)
+		{
+			for (int j = 0; j < c; j++)
+			{
+				int grval = img1.at<uchar>(i, j) * img2.at<uchar>(i, j);
+				if (grval > max_val)
+					max_val = grval;
+				if (grval < min_val)
+					min_val = grval;
+			}
+		}
+		for (int i = 0; i < r; i++)
+		{
+			for (int j = 0; j < c; j++)
+			{
+				int grval = img1.at<uchar>(i, j) * img2.at<uchar>(i, j);
+				if (max_val == 0 && min_val == 0)
+					grval = 0;
+				else
+					grval = ((grval - min_val) * 255) / (max_val - min_val);
+				final_img.at<uchar>(i, j) = grval;
+			}
+		}
+	}
+	return final_img;
+}
+
+Mat imDIVIDE(const Mat &img1, const Mat &img2, int map_fn)
+{
+	int c = img1.cols;
+	int r = img1.rows;
+
+	Mat final_img(r, c, CV_8U);
+	if (map_fn == 1)
+	{
+		for (int i = 0; i < r; i++)
+		{
+			for (int j = 0; j < c; j++)
+			{
 				double grval;
-				if(s2.val[0]!=0)
-					grval = s1.val[0]/s2.val[0];
+				if (img2.at<uchar>(i, j) != 0)
+					grval = static_cast<double>(img1.at<uchar>(i, j)) / img2.at<uchar>(i, j);
 				else
-					grval = s1.val[0]/.5;
-				if(grval > 255.0)
-					grval=255;
-				s.val[0] = (int)(grval);
-				cvSet2D(final_img,i,j,s);
+					grval = static_cast<double>(img1.at<uchar>(i, j)) / 0.5;
+				if (grval > 255.0)
+					grval = 255.0;
+				final_img.at<uchar>(i, j) = static_cast<int>(grval);
 			}
 		}
-
 	}
-	else if(map_fn==2)
+	else if (map_fn == 2)
 	{
-		double min_val=1000000;
-	 	double  max_val=-300;
+		double min_val = 1000000.0;
+		double max_val = -300.0;
 
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
 				double grval;
-				if(s2.val[0]!=0)
-					grval = s1.val[0]/s2.val[0];
+				if (img2.at<uchar>(i, j) != 0)
+					grval = static_cast<double>(img1.at<uchar>(i, j)) / img2.at<uchar>(i, j);
 				else
-					grval = s1.val[0]/.5;
-				if(grval > max_val)
+					grval = static_cast<double>(img1.at<uchar>(i, j)) / 0.5;
+				if (grval > max_val)
 					max_val = grval;
-				if(grval < min_val)
+				if (grval < min_val)
 					min_val = grval;
 			}
 		}
-//		cout<<max_val<<" "<<min_val<<endl;
-		for(int i=0;i<r;i++)
+		for (int i = 0; i < r; i++)
 		{
-			for(int j=0;j<c;j++)
+			for (int j = 0; j < c; j++)
 			{
-				CvScalar s1,s2,s;
-				s1 = cvGet2D(img1,i,j);
-				s2 = cvGet2D(img2,i,j);
 				double grval;
-				if(s2.val[0]!=0)
-					grval = s1.val[0]/s2.val[0];
+				if (img2.at<uchar>(i, j) != 0)
+					grval = static_cast<double>(img1.at<uchar>(i, j)) / img2.at<uchar>(i, j);
 				else
-					grval = s1.val[0]/0.5;
-//				cout<<grval<<" ";
-				if(max_val==0 and min_val==0)
+					grval = static_cast<double>(img1.at<uchar>(i, j)) / 0.5;
+				if (max_val == 0 && min_val == 0)
 					grval = 0;
 				else
 				{
-					grval = ((grval - min_val)*255.0)/(max_val - min_val);
-//					cout<<grval<<" ";
+					grval = ((grval - min_val) * 255.0) / (max_val - min_val);
 				}
-				s.val[0] = (int)(grval);
-//				cout<<s.val[0]<<endl;
-				cvSet2D(final_img,i,j,s);
+				final_img.at<uchar>(i, j) = static_cast<int>(grval);
 			}
 		}
-
 	}
 	return final_img;
 }
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
-
-
-	char *img_nm,*img_typ;
-	IplImage* img1 = cvLoadImage(argv[1],0);
-	
-
-	char *pch,*rem,*f_img;
-	pch = strtok (argv[1],"/");
-	while (pch != NULL)
+	if (argc != 6)
 	{
-		f_img = pch;
-		pch = strtok (NULL, "/");
-
+		cout << "Usage: <program_name> <image1_path> <image2_path> <output_image_path> <operation> <map_fn>" << endl;
+		return -1;
 	}
 
-	img_nm = strtok (f_img,".");
-	img_typ = strtok (NULL, ".");
-
-	int c1 = cvGetSize(img1).width;
-	int r1 = cvGetSize(img1).height;
-
-
-	IplImage* img2 = cvLoadImage(argv[2],0);
-
-	char *imgnm2,*imgtyp2;
-	pch = strtok (argv[2],"/");
-	while (pch != NULL)
+	Mat img1 = imread(argv[1], IMREAD_GRAYSCALE);
+	if (img1.empty())
 	{
-		f_img = pch;
-		pch = strtok (NULL, "/");
-
+		cout << "Error: Could not load image " << argv[1] << endl;
+		return -1;
 	}
 
-	imgnm2 = strtok (f_img,".");
-	imgtyp2 = strtok (NULL, ".");
-	
+	Mat img2 = imread(argv[2], IMREAD_GRAYSCALE);
+	if (img2.empty())
+	{
+		cout << "Error: Could not load image " << argv[2] << endl;
+		return -1;
+	}
 
 	int map_fn = atoi(argv[5]);
-	if(atoi(argv[4])==1)
+	int operation = atoi(argv[4]);
+
+	Mat result_img;
+	switch (operation)
 	{
-		IplImage* add_img=cvCreateImage(cvSize(c1,r1),IPL_DEPTH_8U,1);
-		add_img=imADD(img1,img2,map_fn);
-		cvSaveImage(argv[3],add_img);
-		cvReleaseImage( &add_img );
-	}
-	if(atoi(argv[4])==2)
-	{
-		IplImage* sub_img=cvCreateImage(cvSize(c1,r1),IPL_DEPTH_8U,1);
-		sub_img=imSUB(img1,img2,map_fn);
-		cvSaveImage(argv[3],sub_img);
-		cvReleaseImage( &sub_img );
+	case 1:
+		result_img = imADD(img1, img2, map_fn);
+		break;
+	case 2:
+		result_img = imSUB(img1, img2, map_fn);
+		break;
+	case 3:
+		result_img = imDIFF(img1, img2, map_fn);
+		break;
+	case 4:
+		result_img = imMULTIPLY(img1, img2, map_fn);
+		break;
+	case 5:
+		result_img = imDIVIDE(img1, img2, map_fn);
+		break;
+	default:
+		cout << "Invalid operation" << endl;
+		return -1;
 	}
 
-	if(atoi(argv[4])==3)
-	{
-		IplImage* diff_img=cvCreateImage(cvSize(c1,r1),IPL_DEPTH_8U,1);
-		diff_img=imDIFF(img1,img2,map_fn);
-		cvSaveImage(argv[3],diff_img);
-		cvReleaseImage( &diff_img );
-	}
-
-	if(atoi(argv[4])==4)
-	{
-		IplImage* mul_img=cvCreateImage(cvSize(c1,r1),IPL_DEPTH_8U,1);
-		mul_img=imMULTIPLY(img1,img2,map_fn);
-		cvSaveImage(argv[3],mul_img);
-		cvReleaseImage( &mul_img );
-	}
-	if(atoi(argv[4])==5)
-	{
-		IplImage* div_img=cvCreateImage(cvSize(c1,r1),IPL_DEPTH_8U,1);
-		div_img=imDIVIDE(img1,img2,map_fn);
-		cvSaveImage(argv[3],div_img);
-		cvReleaseImage( &div_img );
-	}
-	cvReleaseImage( &img1 );
-	cvReleaseImage( &img2 );
+	imwrite(argv[3], result_img);
 	return 0;
 }
